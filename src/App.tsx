1a31c4b4d964c1c8327ec334a3b02fa6b6556185
Fanta335 <fantayusuke5210@gmail.com>
2022-01-28 10:20:00 +0900

    add workflow file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const url = process.env.REACT_APP_TEST_URL ? process.env.REACT_APP_TEST_URL : "https://www.google.com/";
 
 function App() {
   return (
@@ -10,14 +12,15 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <p>
+          env url:{" "}
+          <a className="App-link" href={url} target="_blank" rel="noopener noreferrer">
+            {url}
+          </a>
+        </p>
       </header>
     </div>
   );
